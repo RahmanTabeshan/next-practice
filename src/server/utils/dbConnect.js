7228ed3ff73cff4cae68fa92009f402a60c2dc9a
Rahman Tabeshan <rtabeshan33@gmail.com>
2023-01-08 02:24:08 +0330

    push

--- src/server/utils/dbConnect.js
+++ src/server/utils/dbConnect.js
@@ -9,7 +9,7 @@ async function dbConnect() {
         return;
     }
     
-    const db = await mongoose.connect("mongodb+srv://Rahman:hdZWFSSvllNYuWry@cluster0.kpwzoai.mongodb.net/todoDB?retryWrites=true&w=majority", {
+    const db = await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
