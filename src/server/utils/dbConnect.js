61700c38ef0ca9da6ccbfaf10b779d34594b57ca
Rahman Tabeshan <rtabeshan33@gmail.com>
2023-01-08 14:24:02 +0330

    push

--- src/server/utils/dbConnect.js
+++ src/server/utils/dbConnect.js
@@ -2,17 +2,20 @@ import mongoose from "mongoose";
 
 const connection = {};
 
-mongoose.set('strictQuery', true);
- 
+mongoose.set("strictQuery", true);
+
 async function dbConnect() {
     if (connection.isConnected) {
         return;
     }
-    
-    const db = await mongoose.connect(process.env.NEXT_PUBLIC_MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
+
+    const db = await mongoose.connect(
+        `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kpwzoai.mongodb.net/todoDB?retryWrites=true&w=majority`,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    );
     connection.isConnected = db.connections[0].readyState;
 }
 
